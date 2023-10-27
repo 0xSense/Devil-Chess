733dbe4d3c222725883a53a38a89e0b0443311bd
Spencer-Trivette <sdtrivette@gmail.com>
2023-10-27 17:46:50 -0400

    Title Screen Rework

--- main_menu.gd
+++ main_menu.gd
@@ -13,7 +13,7 @@ func _ready():
 func _process(delta):
 	pass
 
-
+# Make sure this is sending to the chessboard scene when final build is being made
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://chess_board.tscn")
 
